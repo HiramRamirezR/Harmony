fb7f3b71523434eae52ac0cd1b7320036dad2884
hiramramirez <hiram.j.ramirez@gmail.com>
2023-03-01 18:52:31 -0600

    create drawing nodes in Harmony practice

---
+++ Create_Drawing_Nodes.js
@@ -0,0 +1,16 @@
+function createDrawingNodes() {
+    var selectedNodes = selection.numberOfNodesSelected();
+    if (selectedNodes == 1) {
+        for (var n = 0; n < selectedNodes; n++) {
+            var currentNode = selection.selectedNode(n);
+            var posX = node.coordX(currentNode);
+            var posY = node.coordY(currentNode);
+
+            for (var i = 0; i < 4; i++) {
+                var newNode = node.add(parentNode(currentNode), "drawing_" + (i + 1), "READ", posX + (120 * i), posY + 100, 0)
+            }
+        } 
+    } else {
+        MessageBox.information("You must select only 1 Composite node.");
+    }
+}
